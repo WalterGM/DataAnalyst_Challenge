9647b2b4b101920f337e2ffe7300495670426ec1
waltg <waltgm@outlook.com>
2024-02-23 01:06:33 -0600

    Adding columns names to constants.conf

--- IngestJob/ingest_job.py
+++ IngestJob/ingest_job.py
@@ -6,18 +6,18 @@ import os
 if __name__ == "__main__":
     config_file = os.path.dirname(os.path.dirname(os.path.abspath(__name__))) + "\\resources\\constants.conf"
     config = ConfigParser()
-    config.read(config_file)
+    config.read(config_file)v
 
     basePath = config['URLS'].get('BasePath')
     files = config['FILE_NAMES'].get("files").split(",")
-    engine = create_engine('postgresql://postgres:admin@localhost/challenge')
+    engine = create_engine('postgresql://postgres:admin@localhost:5432/challenge')
     hdr = {'User-Agent':  'email@email.com'} ## fake email to send the http request
 
     for file in files:
         df_data = pd.read_csv(basePath + file, sep="\t", storage_options=hdr)
         ##removing tabulation from columns
         new_columns = {x: x.strip() for x in df_data.columns}
-        df_data = df_data.rename(columns=new_columns)
+        df_data = df_data.rename(columns=new_columns)[config['COLUMNS'].get(file).split(",")]
         df_data.to_sql(file, engine, index=False, if_exists='append')
         print(f"{file} successfully load to database")
         ##remove from memory
